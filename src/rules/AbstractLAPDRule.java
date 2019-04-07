b0b06a9f23b2d17a137faa9327fd0df15813fb95
rublejn <ruble.jn@gmail.com>
2019-04-07 16:28:16 +0200

    Adding rules and ruleset

---
+++ src/rules/AbstractLAPDRule.java
@@ -0,0 +1,308 @@
+package rules;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceBodyDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTResultType;
+import net.sourceforge.pmd.lang.java.ast.ASTType;
+import net.sourceforge.pmd.lang.java.ast.ASTTypeArgument;
+import net.sourceforge.pmd.lang.java.ast.Comment;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+import net.sourceforge.pmd.properties.PropertyDescriptor;
+import net.sourceforge.pmd.properties.PropertyFactory;
+import utils.CustomDictionary;
+import utils.CustomParser;
+
+import utils.Splitter;
+
+public abstract class AbstractLAPDRule extends AbstractJavaRule {
+
+	protected static List<String> collectionKeywords;
+	protected static List<String> returningKeywords;
+	protected static List<String> ignoreForAntonymsKeywords;
+	protected static List<String> validatingKeywords;
+	protected static List<String> ignoreForConditionKeywords;
+	protected static List<String> aggregationKeywords;
+	protected static List<String> predicateKeywords;
+	
+	public static String[] test = new String[]{"toto", "tutu"};
+
+	protected final static PropertyDescriptor<List<String>> COLLECTION_KEYWORDS 
+    = PropertyFactory.stringListProperty("collectionKeywords")
+    	.desc("keywords to use for collection objects")
+    	.defaultValue(Arrays.asList("buf","buffer","collection","set","array","hash",
+  			"linked","vec","vect","vector","list","iterator","iterable","table","map",
+  			"mapping","stack","hashmap","hashtable","arraylist","queue","alphabet","tree"))
+    	.build();
+	
+
+	protected final static PropertyDescriptor<List<String>> RETURNING_KEYWORDS
+		= PropertyFactory.stringListProperty("returningKeywords")
+			.desc("keywords to use for returning objects")
+			.defaultValue(Arrays.asList("get", "return"))
+			.build();
+
+	protected final static PropertyDescriptor<List<String>> IGNORE_FOR_ANTONYMS_KEYWORDS 
+		= PropertyFactory.stringListProperty("ignoreForAntonymsKeywords")
+			.desc("keywords to ignore for antonym relations")
+			.defaultValue(Arrays.asList("query", "result", "range", "bar"))
+			.build();
+
+	protected final static PropertyDescriptor<List<String>> VALIDATING_KEYWORDS
+		= PropertyFactory.stringListProperty("validatingKeywords")
+			.desc("keywords to use for validating methods")
+			.defaultValue(Arrays.asList("validate", "check", "ensure"))
+			.build();
+
+	protected final static PropertyDescriptor<List<String>> IGNORE_FOR_CONDITION_KEYWORDS
+		= PropertyFactory.stringListProperty("ignoreForConditionKeywords")
+			.desc("terms to ignore as condition keywords")
+			.defaultValue(Arrays.asList("ed if", "if you"))
+			.build();
+
+	protected final static PropertyDescriptor<List<String>> AGGREGATION_KEYWORDS
+		= PropertyFactory.stringListProperty("aggregationKeywords")
+			.desc("aggregation keywords")
+			.defaultValue(Arrays.asList("count", "number", "length", "width", "nb", "sum",
+				"cnt", "cntr"))
+			.build();
+
+	protected final static PropertyDescriptor<List<String>> PREDICATE_KEYWORDS
+		= PropertyFactory.stringListProperty("predicateKeywords")
+			.desc("predicate keywords")
+			.defaultValue(Arrays.asList("is", "has"))
+			.build();
+
+	protected final static String SIGNATURE = "Signature: {0}.{1}: {2}";
+
+	protected AbstractLAPDRule() {
+		definePropertyDescriptor(COLLECTION_KEYWORDS);
+		definePropertyDescriptor(RETURNING_KEYWORDS);
+		definePropertyDescriptor(IGNORE_FOR_ANTONYMS_KEYWORDS);
+		definePropertyDescriptor(VALIDATING_KEYWORDS);
+		definePropertyDescriptor(IGNORE_FOR_CONDITION_KEYWORDS);
+		definePropertyDescriptor(AGGREGATION_KEYWORDS);
+		definePropertyDescriptor(PREDICATE_KEYWORDS);
+	}
+
+	protected static void init() {
+		CustomParser.getInstance();
+		CustomDictionary.getInstance();
+	}
+
+	@Override
+	public void start(RuleContext ctx) {
+		collectionKeywords = getProperty(COLLECTION_KEYWORDS);
+		returningKeywords = getProperty(RETURNING_KEYWORDS);
+		ignoreForAntonymsKeywords = getProperty(IGNORE_FOR_ANTONYMS_KEYWORDS);
+		validatingKeywords = getProperty(VALIDATING_KEYWORDS);
+		ignoreForConditionKeywords = getProperty(IGNORE_FOR_CONDITION_KEYWORDS);
+		aggregationKeywords = getProperty(AGGREGATION_KEYWORDS);
+		predicateKeywords = getProperty(PREDICATE_KEYWORDS);
+	}
+
+	protected static boolean isCollection(final String nodeType) {
+
+		if (nodeType.contains("[")) {
+			return true;
+		}
+
+		Vector<String> splitted = new Vector<String>();
+		final int index = nodeType.indexOf("<");
+
+		if (index != -1) {
+			splitted = Splitter.variableSplitter(nodeType.substring(0, index));
+		} else {
+			splitted = Splitter.variableSplitter(nodeType);
+		}
+
+		if (splitted.size() <= 0) {
+			return false;
+		}
+
+		for (final String word : collectionKeywords) {
+			if (splitted.lastElement().equalsIgnoreCase(word)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected static boolean containsCollectionKeyword(final String item, final boolean substring) {
+
+		for (final String word : collectionKeywords) {
+
+			if (word.equalsIgnoreCase(item) || substring && item.toLowerCase().contains(word)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected static boolean isReturning(final String methodName) {
+
+		final Vector<String> splitted = Splitter.methodSplitter(methodName);
+
+		for (final String word : returningKeywords) {
+			if (splitted.firstElement().equalsIgnoreCase(word)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected static boolean shouldBeIgnoredFromAntonyms(final String source) {
+
+		for (final String target : ignoreForAntonymsKeywords) {
+			if (source.equalsIgnoreCase(target)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected static boolean isAggregation(final String source) {
+
+		for (final String target : aggregationKeywords) {
+			if (source.equalsIgnoreCase(target)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected static boolean isCommentedCode(final String comment) {
+
+		boolean openPar = false, closePar = false, commentedCodeChar = false, containThrows = false;
+
+		if (comment.contains("(")) {
+			openPar = true;
+			if (comment.endsWith("(")) {
+				commentedCodeChar = true;
+			}
+		}
+		if (comment.contains(")")) {
+			closePar = true;
+			if (comment.endsWith(")")) {
+				commentedCodeChar = true;
+			}
+			if (comment.contains("throws")) {
+				containThrows = true;
+			}
+		}
+		if (comment.endsWith("{")) {
+			commentedCodeChar = true;
+		}
+		if (comment.endsWith("}")) {
+			commentedCodeChar = true;
+		}
+		if (comment.endsWith(";")) {
+			commentedCodeChar = true;
+		}
+		return commentedCodeChar || openPar && closePar && containThrows;
+	}
+
+	/*
+	 * works with method declarations, local variables, parameters...
+	 */
+	protected static String getNodeType(Node node) {
+
+		String literalType = "void";
+
+		Node child = node.jjtGetChild(0);
+
+		/* initialisation */
+		while (!(child instanceof ASTType) && !(child instanceof ASTResultType)) {
+			node = node.jjtGetParent();
+			child = node.jjtGetChild(0);
+		}
+
+		/* an ASTType node has no child if the parent node type is void */
+		if (node.jjtGetChild(0).jjtGetNumChildren() != 0) {
+
+			final ASTType type = node.getFirstDescendantOfType(ASTType.class);
+			final String lastToken = type.jjtGetLastToken().toString();
+			literalType = lastToken;
+
+			if ("]".equals(lastToken)) { // special case for an array
+				literalType = type.jjtGetChild(0).jjtGetChild(0).getImage() + "[]";
+			}
+			if (">".equals(lastToken)) {
+				literalType = type.jjtGetChild(0).jjtGetChild(0).getImage() + "<";
+				final List<ASTTypeArgument> arguments = type.findDescendantsOfType(ASTTypeArgument.class);
+				if (!arguments.isEmpty()) {
+					for (ASTTypeArgument argument : arguments) {
+
+						String argumentType = argument.jjtGetChild(0).jjtGetChild(0).getImage();
+						if ("]".equals(argument.jjtGetLastToken().toString())) {
+							argumentType = argumentType + "[]";
+						}
+						literalType = literalType + argumentType + ", ";
+					}
+					literalType = literalType.substring(0, literalType.length() - 2) + ">";
+				}
+
+			}
+
+		}
+
+		return literalType;
+	}
+
+	protected final static Vector<Comment> getNodeComments(final Node node, List<Comment> comments) {
+
+		final Vector<Comment> result = new Vector<Comment>();
+		final Node body = node.getFirstParentOfType(ASTClassOrInterfaceBodyDeclaration.class);
+		final int index = body.jjtGetChildIndex();
+
+		if (index > 0) {
+			/*
+			 * case where the current node is not the first from its type to be declared in
+			 * the body
+			 */
+			final int numChild = body.jjtGetParent().jjtGetChild(index - 1).jjtGetChild(0).jjtGetNumChildren();
+			final Node previous = body.jjtGetParent().jjtGetChild(index - 1).jjtGetChild(0).jjtGetChild(numChild - 1);
+
+			if (previous != null) {
+				for (Comment comment : comments) {
+					if (previous.getEndLine() < comment.getBeginLine() && node.getBeginLine() >= comment.getBeginLine()
+							&& !isCommentedCode(comment.toString())) {
+
+						result.add(comment);
+					}
+				}
+			}
+		} else {
+			/* case for the first declared variable/method in the body. */
+			final Node sup = body.jjtGetParent();
+
+			if (sup != null) {
+				for (final Comment comment : comments) {
+					if (sup.getBeginLine() < comment.getBeginLine() && node.getBeginLine() >= comment.getBeginLine()
+							&& !isCommentedCode(comment.toString())) {
+
+						result.add(comment);
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+	protected static String toString(final List<Comment> comments) {
+
+		String result = "";
+		for (final Comment comment : comments) {
+			result = result + comment.toString() + " ";
+		}
+		if (result.endsWith(" ")) {
+			result = result.substring(0, result.length() - 1);
+		}
+		return result;
+	}
+
+}
---
+++ src/rules/AttributesLAPDRule.java
@@ -0,0 +1,311 @@
+
+package rules;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Vector;
+
+import net.didion.jwnl.JWNLException;
+import net.didion.jwnl.data.POS;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit;
+import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclarator;
+import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
+import net.sourceforge.pmd.lang.java.ast.Comment;
+import net.sourceforge.pmd.properties.PropertyDescriptor;
+import net.sourceforge.pmd.properties.PropertyFactory;
+import utils.CustomDictionary;
+import utils.CustomParser;
+import utils.Splitter;
+
+public class AttributesLAPDRule extends AbstractLAPDRule {
+
+	private boolean collectionTypeSingleNameAttribute;
+	private boolean booleanAttributeNameNotType;
+	private boolean singleTypeCollectionNameAttribute;
+	private boolean oppositeAttributeNameAndType;
+
+	private static final PropertyDescriptor<Boolean> COLLECTION_TYPE_SINGLE_NAME_ATTRIBUTE
+		= PropertyFactory.booleanProperty("collectionTypeSingleNameAttribute")
+			.desc("Checks attributes with collection type and single object name").defaultValue(true)
+			.build();
+
+	private static final PropertyDescriptor<Boolean> BOOLEAN_ATTRIBUTE_NAME_NOT_TYPE
+		= PropertyFactory.booleanProperty("booleanAttributeNameNotType")
+			.desc("Checks attributes whose name suggests boolean but type isn't").defaultValue(true)
+			.build();
+
+	private static final PropertyDescriptor<Boolean> SINGLE_TYPE_COLLECTION_NAME_ATTRIBUTE
+		= PropertyFactory.booleanProperty("singleTypeCollectionNameAttribute")
+			.desc("Checks attributes with single instance type and collection object name")
+			.defaultValue(true).build();
+
+	private static final PropertyDescriptor<Boolean> OPPOSITE_ATTRIBUTE_NAME_AND_TYPE
+		= PropertyFactory.booleanProperty("oppositeAttributeNameAndAttributeType")
+			.desc("Checks antonym relations between terms in attribute type and attribute name")
+			.defaultValue(true).build();
+
+	public AttributesLAPDRule() {
+		definePropertyDescriptor(COLLECTION_TYPE_SINGLE_NAME_ATTRIBUTE);
+		definePropertyDescriptor(BOOLEAN_ATTRIBUTE_NAME_NOT_TYPE);
+		definePropertyDescriptor(SINGLE_TYPE_COLLECTION_NAME_ATTRIBUTE);
+		definePropertyDescriptor(OPPOSITE_ATTRIBUTE_NAME_AND_TYPE);
+
+	}
+
+	@Override
+	public void start(RuleContext ctx) {
+		collectionTypeSingleNameAttribute = getProperty(COLLECTION_TYPE_SINGLE_NAME_ATTRIBUTE);
+		booleanAttributeNameNotType = getProperty(BOOLEAN_ATTRIBUTE_NAME_NOT_TYPE);
+		singleTypeCollectionNameAttribute = getProperty(SINGLE_TYPE_COLLECTION_NAME_ATTRIBUTE);
+		oppositeAttributeNameAndType = getProperty(OPPOSITE_ATTRIBUTE_NAME_AND_TYPE);
+	}
+
+	private static Vector<Comment> getVariableComments(final ASTVariableDeclarator variable,
+			List<Comment> comments) {
+		return getNodeComments(variable, comments);
+	}
+
+	private static String getVariableType(final ASTVariableDeclarator variable) {
+
+		return getNodeType(variable);
+	}
+
+	public Object visit(ASTVariableDeclarator variable, Object data) {
+
+		String variableName;
+		String variableType;
+		final String className = variable.getFirstParentOfType(ASTClassOrInterfaceDeclaration.class)
+				.getImage();
+		final List<Comment> comments = variable.getFirstParentOfType(ASTCompilationUnit.class)
+				.getComments();
+		List<String> result = new ArrayList<String>();
+
+		init();
+
+		variableType = getVariableType(variable);
+		variableName = variable.getFirstChildOfType(ASTVariableDeclaratorId.class).getImage();
+
+		try {
+
+			// D1
+			if (detectCollectionTypeSingleNameAttribute(variableName, variableType)) {
+
+				addViolationWithMessage(data, variable,
+						"LAPD - D1: Attribute type suggests multiple objects but the name suggests single objects. "
+								+ SIGNATURE,
+						new Object[] { className, variableName, variableType });
+			}
+
+			// D2
+			if (detectBooleanAttributeNameNotType(variableName, variableType)) {
+
+				addViolationWithMessage(data, variable,
+						"LAPD - D2: Attribute name is predicate but type is not Boolean. " + SIGNATURE,
+						new Object[] { className, variableName, variableType });
+			}
+
+			// E1
+			if (detectSingleTypeCollectionNameAttribute(variableName, variableType)) {
+
+				addViolationWithMessage(data, variable,
+						"LAPD - E1: Attribute type suggests single object but the name suggests multiple objects. "
+								+ SIGNATURE,
+						new Object[] { className, variableName, variableType });
+			}
+
+			// F1
+			result = detectOppositeAttributeNameAndAttributeType(variableName, variableType);
+			if (result != null) {
+
+				addViolationWithMessage(data, variable,
+						"LAPD - F1: Antonym relation between '{3}' in attribute name and '{4}' in attribute type. "
+								+ SIGNATURE,
+						new Object[] { className, variableName, variableType, result.get(0), result.get(1) });
+			}
+
+			// F2
+			result = detectOppositeCommentAndAttributeSignature(variable, variableName, variableType,
+					comments);
+			if (result != null) {
+
+				addViolationWithMessage(data, variable,
+						"LAPD - F2: Antonym relation between variable signature and comment term: "
+								+ "''{3}'' in comments is an antonym of ''{4}''." + SIGNATURE,
+						new Object[] { className, variableName, variableType, result.get(0), result.get(1) });
+			}
+
+		} catch (JWNLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return data;
+
+	}
+
+	// D1
+	private boolean detectCollectionTypeSingleNameAttribute(final String variableName,
+			final String variableType) {
+
+		if (isCollection(variableType) && !isCollection(variableName)) {
+
+			final Vector<String> splittedVariableName = Splitter.variableSplitter(variableName);
+
+			if (splittedVariableName.size() > 0) {
+
+				final String lastPOS = CustomParser.getPOS(splittedVariableName.lastElement(),
+						splittedVariableName);
+
+				if (lastPOS != null && lastPOS.equalsIgnoreCase("NN")
+						&& !splittedVariableName.lastElement().endsWith("s")
+						&& !CustomParser.hasPOS("NNS", splittedVariableName)) {
+
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	// D2
+	private boolean detectBooleanAttributeNameNotType(final String variableName,
+			final String variableType) throws JWNLException {
+
+		if (!variableType.equalsIgnoreCase("boolean")) {
+
+			final Vector<String> splittedVariableName = Splitter.variableSplitter(variableName);
+
+			if (splittedVariableName.size() > 0) {
+
+				final String firstPOS = CustomParser.getPOS(splittedVariableName.firstElement(),
+						splittedVariableName);
+				final String lastPOS = CustomParser.getPOS(splittedVariableName.lastElement(),
+						splittedVariableName);
+
+				if ("VBG".equalsIgnoreCase(lastPOS)
+						&& !CustomDictionary.hasIndexWordForPos(splittedVariableName.lastElement(), POS.NOUN)
+						|| firstPOS != null && "VBZ".equalsIgnoreCase(firstPOS) && !CustomDictionary
+								.hasIndexWordForPos(splittedVariableName.firstElement(), POS.NOUN)) {
+
+					return true;
+				}
+			}
+			return false;
+		}
+		return false;
+	}
+
+	// E1
+	private boolean detectSingleTypeCollectionNameAttribute(final String variableName,
+			final String variableType) {
+
+		if (!isCollection(variableType) && !isCollection(variableName)) {
+
+			final Vector<String> splittedVariableName = Splitter.variableSplitter(variableName);
+			final Vector<String> splittedVariableType = Splitter.variableSplitter(variableType);
+
+			if (splittedVariableName.size() > 0 && splittedVariableType.size() > 0) {
+
+				for (final String termInType : splittedVariableType) {
+					if (containsCollectionKeyword(termInType, true)) {
+						return false;
+					}
+				}
+
+				for (final String termInName : splittedVariableName) {
+					if (isAggregation(termInName)) {
+						return false;
+					}
+				}
+
+				if (!CustomParser.hasPOS("NNS", splittedVariableType)
+						&& CustomParser.hasPOS("NNS", splittedVariableName)
+						&& splittedVariableName.lastElement().endsWith("s")
+						&& !CustomParser.getPOS(splittedVariableName.firstElement(), splittedVariableName)
+								.startsWith("VB")) {
+
+					return true;
+				}
+			}
+			return false;
+		}
+		return false;
+	}
+
+	// F1
+	private List<String> detectOppositeAttributeNameAndAttributeType(final String variableName,
+			final String variableType) throws JWNLException {
+
+		List<String> result = new ArrayList<String>();
+
+		final Vector<String> splittedVariableType = Splitter.variableSplitter(variableType);
+		final Vector<String> splittedVariableName = Splitter.variableSplitter(variableName);
+
+		for (final String termInName : splittedVariableName) {
+
+			if (!shouldBeIgnoredFromAntonyms(termInName)) {
+
+				for (final String termInType : splittedVariableType) {
+
+					if (!shouldBeIgnoredFromAntonyms(termInType)
+							&& CustomDictionary.haveAntonyms(CustomDictionary.stringToIndexWordSet(termInName),
+									CustomDictionary.stringToIndexWordSet(termInType))) {
+
+						result.add(termInName);
+						result.add(termInType);
+
+						return result;
+					}
+
+				}
+			}
+		}
+		return null;
+	}
+
+	// F2
+	private List<String> detectOppositeCommentAndAttributeSignature(
+			final ASTVariableDeclarator variable, final String variableName, final String variableType,
+			List<Comment> comments) throws JWNLException {
+
+		final List<String> result = new ArrayList<String>();
+		final String variableComments = toString(getVariableComments(variable, comments));
+		final Vector<String> splittedVariableName = Splitter.variableSplitter(variableName);
+		final Vector<String> splittedVariableType = Splitter.variableSplitter(variableType);
+		final Set<String> splittedVariableSignature = new HashSet<String>();
+
+		splittedVariableSignature.addAll(splittedVariableType);
+		splittedVariableSignature.addAll(splittedVariableName);
+
+		if (!variableComments.equals("") && variableComments != null) {
+
+			final Vector<String> splittedVariableComments = Splitter.commentSplitter(variableComments);
+
+			for (final String commentWord : splittedVariableComments) {
+
+				if (!shouldBeIgnoredFromAntonyms(commentWord)) {
+
+					for (final String signatureWord : splittedVariableSignature) {
+
+						if (CustomDictionary.haveAntonyms(CustomDictionary.stringToIndexWordSet(commentWord),
+								CustomDictionary.stringToIndexWordSet(signatureWord))
+								&& !commentWord.equalsIgnoreCase(signatureWord)
+								&& !shouldBeIgnoredFromAntonyms(signatureWord)) {
+
+							result.add(commentWord);
+							result.add(signatureWord);
+							return result;
+
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+}
